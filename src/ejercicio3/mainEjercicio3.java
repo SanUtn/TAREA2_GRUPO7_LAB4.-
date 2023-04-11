6dd79021446a48cebe0aaba292dc2a9d01dde29a
Veronica <veronica.carbonari@gmail.com>
2023-04-11 08:21:57 -0300

    Le ponemos datos a los objetos creados.
    Le ponemos datos da los objetos creados.

--- src/ejercicio3/mainEjercicio3.java
+++ src/ejercicio3/mainEjercicio3.java
@@ -7,29 +7,30 @@ public class mainEjercicio3 {
 	
 	public static void main(String[] args) {
 		
-	ArrayList <Edificio> listaEdificios = new ArrayList <Edificio>();
-	Polideportivo p1 = new Polideportivo();
-	Polideportivo p2 = new Polideportivo();
-	Polideportivo p3 = new Polideportivo();
-
-	Oficina o1 = new Oficina();
-	Oficina o2 = new Oficina();
+		//CREO UN ARRAYLIST, LOS OBJETOS, Y LOS AGREGO AL ARRAYLIST
+		ArrayList <Edificio> listaEdificios = new ArrayList <Edificio>();
+		Polideportivo p1 = new Polideportivo(80, "Centro", 3);
+		Polideportivo p2 = new Polideportivo(120,"Rio",2);
+		Polideportivo p3 = new Polideportivo(220, "Industrial",4);
 	
-	listaEdificios.add(p1);
-	listaEdificios.add(p2);
-	listaEdificios.add(p3);
-
-	listaEdificios.add(o1);
-	listaEdificios.add(o2);
+		Oficina o1 = new Oficina(45, 3);
+		Oficina o2 = new Oficina();
+		
+		listaEdificios.add(p1);
+		listaEdificios.add(p2);
+		listaEdificios.add(p3);
 	
-	System.out.println("Listado de edificios listITERATOR: ");
-	ListIterator <Edificio>it = listaEdificios.listIterator();
-
-	while(it.hasNext()) {//pregunto si hay siguiente para recorrer
-	Edificio e = it.next();//guardo el elemento actual que esta recorriendo.
-	System.out.println(e.toString());
-	}
-
-
+		listaEdificios.add(o1);
+		listaEdificios.add(o2);
+		
+		
+		//IMPRIMO POR CONSOLA EL CONTENIDO DEL ARRAYLIST
+		System.out.println("Listado de edificios listITERATOR: ");
+		ListIterator <Edificio>it = listaEdificios.listIterator();
+	
+		while(it.hasNext()) {//pregunto si hay siguiente para recorrer
+			Edificio e = it.next();//guardo el elemento actual que esta recorriendo.
+			System.out.println(e.toString());
+		}
 	}
 }
